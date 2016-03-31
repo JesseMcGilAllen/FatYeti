40b6efa2fbd64a05d8bf6b5095ca81d7c325a230
Jesse McGil Allen <jesse@jessemcgilallen.com>
2016-03-31 09:30:57 -0500

    fixing packages in StationScraper

--- src/main/java/edu/matc/entjava/fatyeti/controller/StationScraper.java
+++ src/main/java/edu/matc/entjava/fatyeti/controller/StationScraper.java
@@ -1,6 +1,6 @@
-package main.java.com.fatyeti.controller;
+package edu.matc.entjava.fatyeti.controller;
 
-import main.java.com.fatyeti.entity.Station;
+import edu.matc.entjava.fatyeti.entity.Station;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
